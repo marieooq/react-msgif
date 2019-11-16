2e6202fabb58ab4a4aae92eea01d365ccc1eeb98
Marie Otaki <otakimarie@gmail.com>
2019-11-16 16:23:24 +0700

    modified select tag on Mode.js

--- src/Mode.js
+++ src/Mode.js
@@ -2,10 +2,29 @@ import React, { Component } from "react";
 import "./Mode.css";
 
 class Mode extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "note"
+    };
+    this.isMode = this.isMode.bind(this);
+  }
+
+  isMode = e => {
+    this.setState({ value: e.target.value });
+    console.log(this.state.value);
+    return this.state.value;
+  };
+
   render() {
     return (
       <div class="selectdiv">
-        <select name="display-mode" onchange="isMode()" id="mode">
+        <select
+          value={this.state.value}
+          name="display-mode"
+          onChange={this.isMode}
+          id="mode"
+        >
           <option value="developer">Developer</option>
           <option value="neon" selected>
             Neon
