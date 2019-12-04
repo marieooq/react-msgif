b94647d9a58f051116450eace1a8ed7ce3442111
Marie Otaki <otakimarie@gmail.com>
2019-12-03 22:53:53 -0800

    extract ScreenSize.js as a component

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App extends Component {
       recordingFlg: false,
       encoder: "",
       textAreaVal: "",
-      size: "twitter",
       mode: "note",
       outputScreenStatus: "",
       gifAnimation: "",
@@ -51,10 +50,6 @@ class App extends Component {
 
     if (this.state.mode !== prevState.mode) {
       this.switchMode(this.state.mode);
-    }
-
-    if (this.state.size !== prevState.size) {
-      this.changeScreenSize(this.state.size);
     }
   }
 
@@ -85,40 +80,6 @@ class App extends Component {
   setScreenSize = e => {
     this.setState({ size: e.target.value });
   };
-
-  changeScreenSize(size) {
-    let docStyle = document.documentElement.style;
-    let container = document.getElementById("container");
-    let left = document.getElementById("left");
-    let leftLeft = document.getElementById("left-left");
-    let leftRight = document.getElementById("left-right");
-    let textArea = document.getElementById("textareaMsg");
-    let right = document.getElementById("right");
-
-    if (size === "twitter") {
-      container.style.width = "1080px";
-      left.style.width = "512px";
-      left.style.display = "block";
-      left.style.justifyContent = "";
-      leftLeft.style.width = "100%";
-      leftRight.style.width = "100%";
-      textArea.style.marginTop = "0px";
-      right.style.width = "512px";
-      docStyle.setProperty("--screenWidth", "512px");
-      docStyle.setProperty("--screenHeight", "256px");
-    } else if (size === "social") {
-      container.style.width = "1280px";
-      left.style.width = "840px";
-      left.style.display = "flex";
-      left.style.justifyContent = "space-between";
-      leftLeft.style.width = "400px";
-      leftRight.style.width = "400px";
-      textArea.style.marginTop = "20px";
-      right.style.width = "400px";
-      docStyle.setProperty("--screenWidth", "400px");
-      docStyle.setProperty("--screenHeight", "400px");
-    }
-  }
 
   isMode = e => {
     this.setState({ mode: e.target.value });
@@ -455,10 +416,7 @@ class App extends Component {
           <div id="left">
             <div id="left-left">
               <Screen id="display-screen" status="" />
-              <ScreenSize
-                size={this.state.size}
-                onScreenSizeChange={this.setScreenSize}
-              />
+              <ScreenSize />
               <Mode mode={this.state.mode} onModeChange={this.isMode} />
             </div>
             <div id="left-right">
--- src/containers/ScreenSize.js
+++ src/containers/ScreenSize.js
@@ -2,8 +2,53 @@ import React, { Component } from "react";
 import "../components/Mode.css";
 
 class ScreenSize extends Component {
+  state = {
+    size: "twitter"
+  };
+
+  componentDidUpdate(prevState) {
+    if (this.state.size !== prevState.size) {
+      this.changeScreenSize(this.state.size);
+    }
+  }
+
+  changeScreenSize(size) {
+    let docStyle = document.documentElement.style;
+    let container = document.getElementById("container");
+    let left = document.getElementById("left");
+    let leftLeft = document.getElementById("left-left");
+    let leftRight = document.getElementById("left-right");
+    let textArea = document.getElementById("textareaMsg");
+    let right = document.getElementById("right");
+
+    if (size === "twitter") {
+      container.style.width = "1080px";
+      left.style.width = "512px";
+      left.style.display = "block";
+      left.style.justifyContent = "";
+      leftLeft.style.width = "100%";
+      leftRight.style.width = "100%";
+      textArea.style.marginTop = "0px";
+      right.style.width = "512px";
+      docStyle.setProperty("--screenWidth", "512px");
+      docStyle.setProperty("--screenHeight", "256px");
+    } else if (size === "social") {
+      container.style.width = "1280px";
+      left.style.width = "840px";
+      left.style.display = "flex";
+      left.style.justifyContent = "space-between";
+      leftLeft.style.width = "400px";
+      leftRight.style.width = "400px";
+      textArea.style.marginTop = "20px";
+      right.style.width = "400px";
+      docStyle.setProperty("--screenWidth", "400px");
+      docStyle.setProperty("--screenHeight", "400px");
+    }
+  }
+
   handleScreenSize = e => {
-    this.props.onScreenSizeChange(e);
+    this.setState({ size: e.target.value });
+    this.changeScreenSize(e);
   };
 
   render() {
@@ -11,6 +56,7 @@ class ScreenSize extends Component {
     return (
       <div className="selectdiv">
         <select
+          size={this.state.size}
           value={ScreenSizeVal}
           name="screen-size"
           onChange={this.handleScreenSize}
