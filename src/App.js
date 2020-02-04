e7fe75275062fb7d662977d5fdf3449166f30221
Marie Otaki <otakimarie@gmail.com>
2020-02-03 20:43:31 -0800

    add product hunt stecker

--- src/App.js
+++ src/App.js
@@ -239,6 +239,17 @@ export default class App extends Component {
     return (
       <div id="container">
         <ModalWindow />
+        <a
+          href="https://www.producthunt.com/posts/msgif?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-msgif"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=183178&theme=light"
+            alt="Msgif - A GIF generator that converts your message into a GIF | Product Hunt Embed"
+            style={{ width: "250px", height: "54px" }}
+          />
+        </a>
         <div id="main"></div>
         <Notification />
         <header>
