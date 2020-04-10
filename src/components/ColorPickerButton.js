705e7e59cddedb191e19961b12a3b37251784b09
Marie Otaki <otakimarie@gmail.com>
2020-04-10 11:32:46 -0700

    enables to update the button message according to the value of state

--- src/components/ColorPickerButton.js
+++ src/components/ColorPickerButton.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 import store from '../reducers/store';
 
 class ColorPickerButton extends Component {
+  state = {
+    buttonContent: 'Open the color picker'
+  };
+
+  componentDidUpdate(prevState) {
+    if (this.state.buttonContent !== prevState.buttonContent) {
+      console.log('state is changed!!!');
+    }
+  }
   handleSketchPicker = e => {
     if (store.getState().isColorPicker) {
       this.props.isColorPickerFalse();
       console.log(`isColorPicker: ${store.getState().isColorPicker}`);
+      this.setState({ buttonContent: 'Open the color picker' });
     } else {
       this.props.isColorPickerTrue();
       console.log(`isColorPicker: ${store.getState().isColorPicker}`);
+      this.setState({ buttonContent: 'Close the color picker' });
     }
   };
 
   render() {
     return (
       <button onClick={this.handleSketchPicker}>
-        Pick up backgrond color.
+        {this.state.buttonContent}
       </button>
     );
   }
