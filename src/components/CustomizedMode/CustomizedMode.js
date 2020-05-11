b7e03e402b06c88ffc2de18e6844d3b5600776e6
Marie Otaki <otakimarie@gmail.com>
2020-05-11 13:04:24 -0700

    modify the files

--- src/components/CustomizedMode/CustomizedMode.js
+++ src/components/CustomizedMode/CustomizedMode.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import store from '../../reducers/store';
 import BackgroundColorPickerButtonContainer from './CustomizedModeComponents/BackgroundColorPickerButtonContainer';
 import FontColorPickerButtonContainer from './CustomizedModeComponents/FontColorPickerButtonContainer';
+import './CustomizedMode.css';
 
 class CustomizedMode extends Component {
   render() {
     if (store.getState().mode === 'customized') {
       return (
-        <div>
+        <div className="color_picker_button_wrapper">
           <BackgroundColorPickerButtonContainer />
           <FontColorPickerButtonContainer />
         </div>
