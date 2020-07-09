e512068182f7e9a1111041a549f5c3d0a2fbe9d6
Marie Otaki <otakimarie@gmail.com>
2020-07-09 00:03:59 -0700

    extract the product hunt stecker as a new component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ProductHuntStecker from './components/ProductHuntStecker';
 import Description from './components/Description';
 import Note from './components/Note';
 import OutputScreen from './components/OutputScreen';
@@ -245,23 +246,7 @@ export default class App extends Component {
     return (
       <div id="container">
         <ModalWindow />
-        <a
-          href="https://www.producthunt.com/posts/msgif?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-msgif"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=183178&theme=light"
-            alt="Msgif - A GIF generator that converts your message into a GIF | Product Hunt Embed"
-            style={{
-              width: '250px',
-              height: '54px',
-              display: 'block',
-              marginTop: '1%',
-              marginLeft: '1%'
-            }}
-          />
-        </a>
+        <ProductHuntStecker />
         <div id="main"></div>
         <Notification />
         <header>
---
+++ src/components/ProductHuntStecker.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ProductHuntStecker = () => {
+  return (
+    <a
+      href="https://www.producthunt.com/posts/msgif?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-msgif"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=183178&theme=light"
+        alt="Msgif - A GIF generator that converts your message into a GIF | Product Hunt Embed"
+        style={{
+          width: '250px',
+          height: '54px',
+          display: 'block',
+          marginTop: '1%',
+          marginLeft: '1%'
+        }}
+      />
+    </a>
+  );
+};
+
+export default ProductHuntStecker;
