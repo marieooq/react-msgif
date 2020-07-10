4ac04a7c66840fd6f9a99dab4d5b20233d404f31
Marie Otaki <otakimarie@gmail.com>
2020-07-10 16:33:00 -0700

    remove unneccessary codes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import ProductHuntStecker from './components/ProductHuntStecker';
-import Step1 from './components/Step1';
-import Step2Container from './components/Step2Container';
-import Step3 from './components/Step3';
 import './App.css';
 import ModalWindow from './components/ModalWindow';
 import Notification from './containers/Notification';
@@ -11,10 +8,6 @@ import Steps from './components/Steps';
 import Footer from './components/Footer';
 
 export default class App extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
-
   render() {
     return (
       <div id="container">
