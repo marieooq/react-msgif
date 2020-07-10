f43d924ccce544a806c1460085fd85e26f0f1b01
Marie Otaki <otakimarie@gmail.com>
2020-07-10 16:30:08 -0700

    extract all of the steps as a component named Steps

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import ProductHuntStecker from './components/ProductHuntStecker';
-// import Description from './components/Description';
-// import OutputScreen from './components/OutputScreen';
 import Step1 from './components/Step1';
 import Step2Container from './components/Step2Container';
 import Step3 from './components/Step3';
-// import Download from './components/Download';
 import './App.css';
-import store from './reducers/store';
 import ModalWindow from './components/ModalWindow';
 import Notification from './containers/Notification';
+import Steps from './components/Steps';
 import Footer from './components/Footer';
 
 export default class App extends Component {
@@ -25,15 +22,7 @@ export default class App extends Component {
         <ProductHuntStecker />
         <Notification />
         <Header />
-
-        <div id="inner">
-          <Step1 />
-
-          <Step2Container />
-
-          <Step3 />
-        </div>
-
+        <Steps />
         <Footer />
       </div>
     );
