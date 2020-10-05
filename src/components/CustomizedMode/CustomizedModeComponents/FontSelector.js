dee04c9e872c53344b370f352e7093bb317fa564
Marie Otaki <otakimarie@gmail.com>
2020-10-04 17:34:52 -0700

    change the class component to a function component in FontSelector.js

--- src/components/CustomizedMode/CustomizedModeComponents/FontSelector.js
+++ src/components/CustomizedMode/CustomizedModeComponents/FontSelector.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import store from '../../../reducers/store';
 import '../../Mode.css';
 
-class FontSelector extends Component {
+const FontSelector = (props) => {
   //   font-family: 'Abril Fatface', cursive;
   // font-family: 'Archivo Black', sans-serif;
   // font-family: 'Concert One', cursive;
@@ -24,50 +24,49 @@ class FontSelector extends Component {
   // font-family: 'Ultra', serif;
   // font-family: 'Bangers', cursive;
 
-  handleFontFamily = e => {
-    this.props.changeCutomizedFontFamily(e.target.value);
+  const handleFontFamily = e => {
+    props.changeCutomizedFontFamily(e.target.value);
   };
-  render() {
-    if (store.getState().mode === 'customized') {
-      return (
-        <div className="selectdiv">
-          <label htmlFor="select_customized_font">Font type</label>
-          <select
-            value={store.getState().customizedFontFamily}
-            name="display-mode"
-            onChange={this.handleFontFamily}
-            id="select_customized_font"
-          >
-            <option value="'Comic Neue', cursive">Comic Neue</option>
-            <option value="'Abril Fatface', cursive">Abril</option>
-            <option value="'Archivo Black', sans-serif">Archivo Black</option>
-            <option value="'Cookie', cursive">Cookie</option>
-            <option value="'Gloria Hallelujah', cursive">
-              Gloria Hallelujah
-            </option>
-            <option value="'Gochi Hand', cursive">Gochi Hand</option>
-            <option value="'Handlee', cursive">Handlee</option>
-            <option value="'Kaushan Script', cursive">Kaushan Script</option>
-            <option value="'Long Cang', cursive">Long Cang</option>
-            <option value="'Luckiest Guy', cursive">Luckiest Guy</option>
-            <option value="'Monoton', cursive">Monoton</option>
-            <option value="'Parisienne', cursive">Parisienne</option>
-            <option value="'Permanent Marker', cursive">
-              Permanent Marker
-            </option>
-            <option value="'Prata', serif">Prata</option>
-            <option value="'Sacramento', cursive">Sacramento</option>
-            <option value="'Satisfy', cursive">Satisfy</option>
-            <option value="'Sigmar One', cursive">Sigmar One</option>
-            <option value="'Special Elite', cursive">Special Elite</option>
-            <option value="'Ultra', serif">Ultra</option>
-            <option value="'Bangers', cursive">Bangers</option>
-          </select>
-        </div>
-      );
-    } else {
-      return <></>;
-    }
+
+  if (store.getState().mode === 'customized') {
+    return (
+      <div className="selectdiv">
+        <label htmlFor="select_customized_font">Font type</label>
+        <select
+          value={store.getState().customizedFontFamily}
+          name="display-mode"
+          onChange={handleFontFamily}
+          id="select_customized_font"
+        >
+          <option value="'Comic Neue', cursive">Comic Neue</option>
+          <option value="'Abril Fatface', cursive">Abril</option>
+          <option value="'Archivo Black', sans-serif">Archivo Black</option>
+          <option value="'Cookie', cursive">Cookie</option>
+          <option value="'Gloria Hallelujah', cursive">
+            Gloria Hallelujah
+          </option>
+          <option value="'Gochi Hand', cursive">Gochi Hand</option>
+          <option value="'Handlee', cursive">Handlee</option>
+          <option value="'Kaushan Script', cursive">Kaushan Script</option>
+          <option value="'Long Cang', cursive">Long Cang</option>
+          <option value="'Luckiest Guy', cursive">Luckiest Guy</option>
+          <option value="'Monoton', cursive">Monoton</option>
+          <option value="'Parisienne', cursive">Parisienne</option>
+          <option value="'Permanent Marker', cursive">
+            Permanent Marker
+          </option>
+          <option value="'Prata', serif">Prata</option>
+          <option value="'Sacramento', cursive">Sacramento</option>
+          <option value="'Satisfy', cursive">Satisfy</option>
+          <option value="'Sigmar One', cursive">Sigmar One</option>
+          <option value="'Special Elite', cursive">Special Elite</option>
+          <option value="'Ultra', serif">Ultra</option>
+          <option value="'Bangers', cursive">Bangers</option>
+        </select>
+      </div>
+    );
+  } else {
+    return <></>;
   }
 }
 
