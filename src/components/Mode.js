aeac6caf53a14b9aeec75f65c7c591e992292fc1
Marie Otaki <otakimarie@gmail.com>
2020-10-05 14:21:31 -0700

    make Mode.js back to #fbfaf232

--- src/components/Mode.js
+++ src/components/Mode.js
@@ -1,47 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import store from '../reducers/store';
 import './Mode.css';
 
-const Mode = (props) =>{
-  let displayScreen;
+class Mode extends Component {
+  displayScreen;
 
-  useEffect(() => {
-    displayScreen = document.getElementById('textareaMsg');
-  });
+  componentDidMount() {
+    this.displayScreen = document.getElementById('textareaMsg');
+  }
 
-  useEffect(()=> {
-    switchMode(store.getState().mode)
-  }, [store.getState().mode]);
-
-  useEffect(()=> {
-    switchMode(store.getState().mode)
-  }, [store.getState().customizedBackgroundColor]);
-
-  useEffect(()=> {
-    switchMode(store.getState().mode)
-  }, [store.getState().textAreaVal]);
-
-  useEffect(()=> {
-    switchMode(store.getState().mode)
-  }, [store.getState().isFontColorPicker]);
-  
-  useEffect(()=> {
-    switchMode(store.getState().mode)
-  }, [store.getState().isBackgroundColorPicker]);
-
-  const switchMode = mode => {
-    if (mode !== 'customized') {
-      props.isFontColorPickerFalse();
-      props.isBackgroundColorPickerFalse();
+  componentDidUpdate(prevState) {
+    if (store.getState().mode !== prevState.mode) {
+      this.switchMode(store.getState().mode);
     }
-    changeTextColor(mode);
-    changeTextShadow(mode);
-    changeFontFamily(mode);
-    changeBackground(mode);
+
+    if (
+      store.getState().customizedBackgroundColor !==
+      prevState.customizedBackgroundColor
+    ) {
+      this.switchMode(store.getState().mode);
+    }
+
+    if (store.getState().textAreaVal !== prevState.textAreaVal) {
+      this.switchMode(store.getState().mode);
+    }
+
+    if (store.getState().isFontColorPicker) {
+      this.switchMode(store.getState().mode);
+    }
+
+    if (store.getState().isBackgroundColorPicker) {
+      this.switchMode(store.getState().mode);
+    }
+  }
+  switchMode = mode => {
+    if (mode !== 'customized') {
+      this.props.isFontColorPickerFalse();
+      this.props.isBackgroundColorPickerFalse();
+    }
+    this.changeTextColor(mode);
+    this.changeTextShadow(mode);
+    this.changeFontFamily(mode);
+    this.changeBackground(mode);
   };
 
-  const changeTextColor = mode => {
-    displayScreen.classList.add('coloredText');
+  changeTextColor = mode => {
+    this.displayScreen.classList.add('coloredText');
     const coloredTextClass = document.getElementsByClassName('coloredText');
 
     for (let i = 0; i < coloredTextClass.length; i++) {
@@ -86,7 +90,7 @@ const Mode = (props) =>{
     }
   };
 
-  const changeTextShadow = mode => {
+  changeTextShadow = mode => {
     const returnRanNum = () => {
       const runNum = Math.floor(Math.random() * 360);
       return runNum;
@@ -98,101 +102,101 @@ const Mode = (props) =>{
     };
 
     if (mode === 'neon') {
-      displayScreen.classList.add('textShadow');
+      this.displayScreen.classList.add('textShadow');
       const textShadowClass = document.getElementsByClassName('textShadow');
       for (let i = 0; i < textShadowClass.length; i++) {
         textShadowClass[i].style.textShadow = `2px 2px 15px ${returnRgb()}`;
       }
     } else {
-      if (displayScreen.className.includes('textShadow')) {
-        displayScreen.classList.remove('textShadow');
+      if (this.displayScreen.className.includes('textShadow')) {
+        this.displayScreen.classList.remove('textShadow');
       }
-      displayScreen.style.textShadow = '';
+      this.displayScreen.style.textShadow = '';
     }
   };
 
-  const changeFontFamily = mode => {
+  changeFontFamily = mode => {
     switch (mode) {
       case 'developer':
-        displayScreen.style.fontFamily =
+        this.displayScreen.style.fontFamily =
           '"Ubuntu Mono", monospace, "Kosugi", sans-serif';
         break;
 
       case 'neon':
-        displayScreen.style.fontFamily =
+        this.displayScreen.style.fontFamily =
           '"Lobster", cursive, "Kosugi", sans-serif';
         break;
 
       case 'note':
-        displayScreen.style.fontFamily = '"Noto Serif", serif';
+        this.displayScreen.style.fontFamily = '"Noto Serif", serif';
         break;
 
       case 'pop-yellow':
-        displayScreen.style.fontFamily = "'Anton', 'Kosugi', sans-serif";
+        this.displayScreen.style.fontFamily = "'Anton', 'Kosugi', sans-serif";
         break;
 
       case 'pop-blue':
-        displayScreen.style.fontFamily = "'Anton', 'Kosugi', sans-serif";
+        this.displayScreen.style.fontFamily = "'Anton', 'Kosugi', sans-serif";
         break;
 
       case 'pop-pink':
-        displayScreen.style.fontFamily = "'Anton', 'Kosugi', sans-serif";
+        this.displayScreen.style.fontFamily = "'Anton', 'Kosugi', sans-serif";
         break;
 
       case 'navy':
-        displayScreen.style.fontFamily = "'PT Sans', 'Kosugi', sans-serif";
+        this.displayScreen.style.fontFamily = "'PT Sans', 'Kosugi', sans-serif";
         break;
 
       case 'customized':
-        displayScreen.style.fontFamily = store.getState().customizedFontFamily;
+        this.displayScreen.style.fontFamily = store.getState().customizedFontFamily;
         break;
 
       default:
         console.log('apply for the default font family.');
-        displayScreen.style.fontFamily = '"Noto Serif", serif';
+        this.displayScreen.style.fontFamily = '"Noto Serif", serif';
     }
   };
 
-  const changeBackground = mode => {
+  changeBackground = mode => {
     let margin_background_color;
     switch (mode) {
       case 'developer':
-        displayScreen.style.backgroundColor = 'rgb(12, 5, 32)';
+        this.displayScreen.style.backgroundColor = 'rgb(12, 5, 32)';
         margin_background_color = 'rgb(12, 5, 32)';
         break;
 
       case 'neon':
-        displayScreen.style.backgroundColor = 'rgb(12, 5, 32)';
+        this.displayScreen.style.backgroundColor = 'rgb(12, 5, 32)';
         margin_background_color = 'rgb(12, 5, 32)';
         break;
 
       case 'note':
-        displayScreen.style.backgroundColor = '#fff';
+        this.displayScreen.style.backgroundColor = '#fff';
         margin_background_color = '#fff';
         break;
 
       case 'pop-yellow':
-        displayScreen.style.backgroundColor = '#fef734';
+        this.displayScreen.style.backgroundColor = '#fef734';
         margin_background_color = '#fef734';
         break;
 
       case 'pop-blue':
-        displayScreen.style.backgroundColor = '#34ccfe';
+        this.displayScreen.style.backgroundColor = '#34ccfe';
         margin_background_color = '#34ccfe';
         break;
 
       case 'pop-pink':
-        displayScreen.style.backgroundColor = '#fe34a6';
+        this.displayScreen.style.backgroundColor = '#fe34a6';
         margin_background_color = '#fe34a6';
         break;
 
       case 'navy':
-        displayScreen.style.backgroundColor = '#043364';
+        this.displayScreen.style.backgroundColor = '#043364';
         margin_background_color = '#043364';
         break;
 
       case 'customized':
-        displayScreen.style.backgroundColor = store.getState().customizedBackgroundColor;
+        this.displayScreen.style.backgroundColor = store.getState().customizedBackgroundColor;
         margin_background_color = store.getState().customizedBackgroundColor;
         break;
 
@@ -200,33 +204,35 @@ const Mode = (props) =>{
         console.log('mode is undefined');
     }
 
-    props.marginBackgroundColor(margin_background_color);
+    this.props.marginBackgroundColor(margin_background_color);
   };
 
-  const handleMode = e => {
-    props.changeMode(e.target.value);
+  handleMode = e => {
+    this.props.changeMode(e.target.value);
   };
 
-  return (
-    <div className="selectdiv">
-      <label htmlFor="select_mode">Design style</label>
-      <select
-        value={store.getState().mode}
-        name="display-mode"
-        onChange={handleMode}
-        id="select_mode"
-      >
-        <option value="developer">Developer</option>
-        <option value="neon">Neon</option>
-        <option value="note">Note</option>
-        <option value="pop-yellow">Pop(yellow)</option>
-        <option value="pop-blue">Pop(blue)</option>
-        <option value="pop-pink">Pop(pink)</option>
-        <option value="navy">Pop(navy)</option>
-        <option value="customized">Customized</option>
-      </select>
-    </div>
-  );
+  render() {
+    return (
+      <div className="selectdiv">
+        <label htmlFor="select_mode">Design style</label>
+        <select
+          value={store.getState().mode}
+          name="display-mode"
+          onChange={this.handleMode}
+          id="select_mode"
+        >
+          <option value="developer">Developer</option>
+          <option value="neon">Neon</option>
+          <option value="note">Note</option>
+          <option value="pop-yellow">Pop(yellow)</option>
+          <option value="pop-blue">Pop(blue)</option>
+          <option value="pop-pink">Pop(pink)</option>
+          <option value="navy">Pop(navy)</option>
+          <option value="customized">Customized</option>
+        </select>
+      </div>
+    );
+  }
 }
 
 export default Mode;
